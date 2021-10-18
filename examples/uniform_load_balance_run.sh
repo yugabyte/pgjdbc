888c630c3ba3c78d0db25c1fc8071a3c175ef120
Neeraj Kumar <nkumar@yugabyte.com>
2021-10-18 15:43:35 +0530

    Added Yugabyte license headers in source files.

--- examples/uniform_load_balance_run.sh
+++ examples/uniform_load_balance_run.sh
@@ -1,5 +1,18 @@
 #!/bin/bash                                                
 
+# Copyright (c) YugaByte, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+# in compliance with the License.  You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software distributed under the License
+# is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+# or implied.  See the License for the specific language governing permissions and limitations
+# under the License.
+#
+
 #file: .jdbc_example_app_checker are just used for signaling the java app whether to continue or pause at this point of time
 #file: .notify_shell_script is used to signal shell script from the java whether to continue or pause at this point of time
 
